181b96fdece1d10d25488dff04aed5b2cc013d69
BigEngland <andrew.wardwell@janeirodigital.com>
2018-10-15 12:57:37 -0400

    change photo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'kathleenceramics';
+  src='/src/assets/images/1927.jpg';
+  src2='/src/assets/images/1927head.jpg';
+  imagePath='';
+  ngOnInit(){
+    this.changeImage('boom');
+  }
+  changeImage= function(keyword){ 
+    if (keyword== 'boom'){
+      this.imagePath= this.src;
+    
+    } else {
+      this.imagePath= this.src2;
+    }
+    
+  }
+  
 }
